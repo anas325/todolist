036ac8dfbcaf6ab9d90a249257694acc5d3131d2
anas325 <91235726+anas325@users.noreply.github.com>
2025-12-11 16:53:58 +0100

    Add AppBar component and integrate SafeAreaView; enhance tab navigator with icons and styles

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'; 
 import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; 
+import { Ionicons } from '@expo/vector-icons';
+import AppBar from './screens/appBar';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'; 
+
 import HomeScreen from './screens/HomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
 import SettingsScreen from './screens/SettingsScreen';
@@ -18,9 +22,14 @@ const Tab = createBottomTabNavigator();
 // --- Navigation par pile --- 
 function HomeStack() { 
   return ( 
-    <Stack.Navigator> 
-      <Stack.Screen name="Accueil" component={HomeScreen} /> 
-      <Stack.Screen name="Details" component={DetailsScreen} /> 
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen  name="Accueil"  component={HomeScreen}  options={{ 
+        headerStyle: { backgroundColor: '#007AFF' },    headerTintColor: '#fff',    headerTitleStyle: { 
+    fontWeight: 'bold' }, 
+      }} 
+    />       
+    <Stack.Screen  name="Details"  component={DetailsScreen}  options={{ title: 'Mes Détails Personnalisés' }} 
+/> 
     </Stack.Navigator> 
   ); 
 } 
@@ -28,11 +37,29 @@ function HomeStack() {
 // --- Navigation par onglets --- 
 export default function App() { 
   return ( 
+     <SafeAreaProvider>
+         <SafeAreaView style={{ flex: 1, backgroundColor: '#007AFF' }}>
     <NavigationContainer> 
-      <Tab.Navigator screenOptions={{ headerShown: false }}> 
-        <Tab.Screen name="Maison" component={HomeStack} /> 
-        <Tab.Screen name="Paramètres" component={SettingsScreen} /> 
+       <AppBar /> 
+      <Tab.Navigator 
+screenOptions={{ 
+headerShown: false,          
+// cacher le header 
+tabBarActiveTintColor: 'blue', // couleur de l’onglet 
+tabBarInactiveTintColor: 'gray', // couleur de l’onglet 
+tabBarStyle: { backgroundColor: '#f0f0f0' }, // style de 
+tabBarLabelStyle: { fontSize: 14 }, // style du texte 
+}} 
+> 
+        <Tab.Screen  name="Maison"  component={HomeStack}  options={{ tabBarIcon: ({ color, size 
+}) => (  <Ionicons name="home" size={size} color={color} />     ), 
+}} /> 
+<Tab.Screen name="Paramètres" component={SettingsScreen} options={{ tabBarIcon: ({ 
+color,    size }) => ( <Ionicons name="settings" size={size} color={color} />  ), 
+}}  />
       </Tab.Navigator> 
-    </NavigationContainer> 
+    </NavigationContainer>
+    </SafeAreaView>
+    </SafeAreaProvider> 
   ); 
 }
